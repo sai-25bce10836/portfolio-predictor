72f029bb351683dae88b479eb37b42c3d91ba196
sai-25bce10836 <sai.25bce10836@vitbhopal.ac.in>
2026-08-02 18:55:43 +0530

    update  url to render live livepoint

--- frontend/script.js
+++ frontend/script.js
@@ -77,7 +77,7 @@ document.addEventListener('DOMContentLoaded', () => {
         });
 
         try {
-            const response = await fetch('http://127.0.0.1:8000/analyze', {
+            const response = await fetch('https://portfolio-predictor-s6me.onrender.com', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ stocks: stocks })
